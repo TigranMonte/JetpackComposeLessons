f7984b42869f2c1853ed794287965f83d1281ebf
Tigran <tigranma@gmail.com>
2022-05-13 16:55:59 +0300

    Compose Lessons - Row, text

--- app/src/main/java/ru/tikodvlp/jetpackcomposelessons/MainActivity.kt
+++ app/src/main/java/ru/tikodvlp/jetpackcomposelessons/MainActivity.kt
@@ -3,26 +3,28 @@ package ru.tikodvlp.jetpackcomposelessons
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ru.tikodvlp.jetpackcomposelessons.ui.theme.JetpackComposeLessonsTheme
+import androidx.compose.ui.graphics.Color
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NewText()
+            Row(modifier = Modifier
+                .background(Color.Green)
+                .fillMaxSize(),
+                horizontalArrangement = Arrangement.SpaceAround
+            ){
+                Text(text = "Hello")
+                Text(text = "Tiko")
+                Text(text = "Monte")
+            }
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun NewText() {
-    Text(text = "Hello Moto")
 }

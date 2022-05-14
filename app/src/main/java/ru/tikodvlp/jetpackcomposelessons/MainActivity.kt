43aea6c2f8ceff4fb692d44a0713bf4ae02f2505
Tigran <tigranma@gmail.com>
2022-05-14 19:05:38 +0300

    Compose Lessons - Row and Column, text

--- app/src/main/java/ru/tikodvlp/jetpackcomposelessons/MainActivity.kt
+++ app/src/main/java/ru/tikodvlp/jetpackcomposelessons/MainActivity.kt
@@ -4,10 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
@@ -18,12 +17,26 @@ class MainActivity : ComponentActivity() {
         setContent {
             Row(modifier = Modifier
                 .background(Color.Green)
-                .fillMaxSize(),
-                horizontalArrangement = Arrangement.SpaceAround
+                .fillMaxWidth().fillMaxHeight(0.5f)) {
+            Column( modifier = Modifier
+                .background(Color.Red)
+                .fillMaxWidth(0.5f).fillMaxHeight(),
+                verticalArrangement = Arrangement.SpaceAround,
+                horizontalAlignment = Alignment.CenterHorizontally
             ){
                 Text(text = "Hello")
                 Text(text = "Tiko")
                 Text(text = "Monte")
+            }
+            Column(modifier = Modifier
+                .background(Color.Magenta).fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceAround,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Text(text = "Hello")
+                Text(text = "Tiko")
+                Text(text = "Monte")
+                }
             }
         }
     }

21cfe356f8915d5b4c03fb2634570ca197ddb6a8
Tigran <tigranma@gmail.com>
2022-05-16 17:58:10 +0300

    Compose Lessons - Card, Box

--- app/src/main/java/ru/tikodvlp/jetpackcomposelessons/MainActivity.kt
+++ app/src/main/java/ru/tikodvlp/jetpackcomposelessons/MainActivity.kt
@@ -3,39 +3,49 @@ package ru.tikodvlp.jetpackcomposelessons
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(modifier = Modifier
-                .background(Color.Green)
-                .fillMaxWidth().fillMaxHeight(0.5f)) {
-            Column( modifier = Modifier
-                .background(Color.Red)
-                .fillMaxWidth(0.5f).fillMaxHeight(),
-                verticalArrangement = Arrangement.SpaceAround,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ){
-                Text(text = "Hello")
-                Text(text = "Tiko")
-                Text(text = "Monte")
-            }
-            Column(modifier = Modifier
-                .background(Color.Magenta).fillMaxSize(),
-                verticalArrangement = Arrangement.SpaceAround,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ){
-                Text(text = "Hello")
-                Text(text = "Tiko")
-                Text(text = "Monte")
+            ListItem("Real Madrid", "Football Club")
+        }
+    }
+}
+@Composable
+private fun ListItem(name: String, prof: String) {
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .padding(10.dp),
+    shape = RoundedCornerShape(15.dp),
+    elevation = 5.dp)
+    {
+        Box(){
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(painter = painterResource(id = R.drawable.logo_bw),
+                    contentDescription = "Real", contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(64.dp)
+                        .clip(CircleShape)
+                )
+                Column() {
+                    Text(text = name)
+                    Text(text = prof)
                 }
             }
         }

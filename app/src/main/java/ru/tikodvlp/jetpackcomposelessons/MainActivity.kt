b60ed67790c38c9ca80dc18125e2d6135e288dc5
Tigran <tigranma@gmail.com>
2022-05-17 21:54:01 +0300

    Compose Lessons - State - mutableStateOf

--- app/src/main/java/ru/tikodvlp/jetpackcomposelessons/MainActivity.kt
+++ app/src/main/java/ru/tikodvlp/jetpackcomposelessons/MainActivity.kt
@@ -4,81 +4,63 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-                ListItem("Real Madrid", "Football Club")
-                ListItem("Real Madrid", "Football Club")
-                ListItem("Real Madrid", "Football Club")
-                ListItem("Real Madrid", "Football Club")
-                ListItem("Real Madrid", "Football Club")
-                ListItem("Real Madrid", "Football Club")
-                ListItem("Real Madrid", "Football Club")
-                ListItem("Real Madrid", "Football Club")
-                ListItem("Real Madrid", "Football Club")
-                ListItem("Real Madrid", "Football Club")
-                ListItem("Real Madrid", "Football Club")
-                ListItem("Real Madrid", "Football Club")
-                ListItem("Real Madrid", "Football Club")
-                ListItem("Real Madrid", "Football Club")
-                ListItem("Real Madrid", "Football Club")
-                ListItem("Real Madrid", "Football Club")
+            Box(modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center) {
+                CircleItem()
             }
         }
     }
 }
 @Composable
-private fun ListItem(name: String, prof: String) {
-    Card(modifier = Modifier
-        .fillMaxWidth()
-        .padding(10.dp)
-        .pointerInput(Unit){
-            detectHorizontalDragGestures { change, dragAmount ->
-                Log.d("MyLog", "Long press: $dragAmount")
-            }
+private fun CircleItem() {
+    val counter = remember {
+        mutableStateOf(0)
+    }
+
+    val color = remember {
+        mutableStateOf(Color.Blue)
+    }
+
+    Box(modifier = Modifier
+        .size(100.dp)
+        .background(color = color.value, shape = CircleShape).clickable {
+           when(++counter.value){
+               10 -> color.value = Color.Red
+               20 -> color.value = Color.Green
+           }
         },
-    shape = RoundedCornerShape(15.dp),
-    elevation = 5.dp)
-    {
-        Box(){
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Image(painter = painterResource(id = R.drawable.logo_bw),
-                    contentDescription = "Real", contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .padding(5.dp)
-                        .size(64.dp)
-                        .clip(CircleShape)
-                )
-                Column(modifier = Modifier.padding(start = 16.dp)) {
-                    Text(text = name)
-                    Text(text = prof)
-                }
-            }
-        }
+    contentAlignment = Alignment.Center) {
+        Text(text = counter.value.toString(),
+            style = TextStyle(color = Color.White, fontSize = 20.sp))
     }
 }

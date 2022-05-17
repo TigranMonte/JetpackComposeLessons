66f60a9808011648def0502c820a9a53f4226a09
Tigran <tigranma@gmail.com>
2022-05-17 21:03:55 +0300

    Compose Lessons - Modifier обработчики касаний, нажатий, долгие нажатия, перемещения

--- app/src/main/java/ru/tikodvlp/jetpackcomposelessons/MainActivity.kt
+++ app/src/main/java/ru/tikodvlp/jetpackcomposelessons/MainActivity.kt
@@ -1,18 +1,27 @@
 package ru.tikodvlp.jetpackcomposelessons
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -22,7 +31,17 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column() {
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                ListItem("Real Madrid", "Football Club")
+                ListItem("Real Madrid", "Football Club")
+                ListItem("Real Madrid", "Football Club")
+                ListItem("Real Madrid", "Football Club")
+                ListItem("Real Madrid", "Football Club")
+                ListItem("Real Madrid", "Football Club")
+                ListItem("Real Madrid", "Football Club")
+                ListItem("Real Madrid", "Football Club")
+                ListItem("Real Madrid", "Football Club")
+                ListItem("Real Madrid", "Football Club")
                 ListItem("Real Madrid", "Football Club")
                 ListItem("Real Madrid", "Football Club")
                 ListItem("Real Madrid", "Football Club")
@@ -37,7 +56,12 @@ class MainActivity : ComponentActivity() {
 private fun ListItem(name: String, prof: String) {
     Card(modifier = Modifier
         .fillMaxWidth()
-        .padding(10.dp),
+        .padding(10.dp)
+        .pointerInput(Unit){
+            detectHorizontalDragGestures { change, dragAmount ->
+                Log.d("MyLog", "Long press: $dragAmount")
+            }
+        },
     shape = RoundedCornerShape(15.dp),
     elevation = 5.dp)
     {

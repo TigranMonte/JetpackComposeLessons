1f5733c1c8ebf994eeca6ed725456fb6a97d4fb9
Tigran <tigranma@gmail.com>
2022-05-18 21:11:57 +0300

    Compose Lessons - LazyColumn

--- app/src/main/java/ru/tikodvlp/jetpackcomposelessons/MainActivity.kt
+++ app/src/main/java/ru/tikodvlp/jetpackcomposelessons/MainActivity.kt
@@ -5,25 +5,17 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.*
-import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
-import androidx.compose.foundation.gestures.detectHorizontalDragGestures
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -34,33 +26,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center) {
-                CircleItem()
+            LazyColumn(horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()) {
+                itemsIndexed(listOf("Item 1", "Real", "Mbappe", "FIFA 22")) { index, item ->
+                    Text(text = item, fontSize = 30.sp,
+                        modifier = Modifier.padding(vertical = 10.dp))
+                }
             }
         }
     }
 }
-@Composable
-private fun CircleItem() {
-    val counter = remember {
-        mutableStateOf(0)
-    }
 
-    val color = remember {
-        mutableStateOf(Color.Blue)
-    }
-
-    Box(modifier = Modifier
-        .size(100.dp)
-        .background(color = color.value, shape = CircleShape).clickable {
-           when(++counter.value){
-               10 -> color.value = Color.Red
-               20 -> color.value = Color.Green
-           }
-        },
-    contentAlignment = Alignment.Center) {
-        Text(text = counter.value.toString(),
-            style = TextStyle(color = Color.White, fontSize = 20.sp))
-    }
-}

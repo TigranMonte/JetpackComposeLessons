cbdd080b5577c71b9a5f337e62765fa63e59ddc6
Tigran <tigranma@gmail.com>
2022-11-15 21:34:40 +0300

    WeatherApp

--- wheatherappcompose/src/main/java/ru/tikodvlp/wheatherappcompose/screens/UI.kt
+++ wheatherappcompose/src/main/java/ru/tikodvlp/wheatherappcompose/screens/UI.kt
@@ -1,10 +1,14 @@
 package ru.tikodvlp.wheatherappcompose.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -17,11 +21,26 @@ import ru.tikodvlp.wheatherappcompose.data.WeatherModel
 import ru.tikodvlp.wheatherappcompose.ui.theme.BlueLight
 
 @Composable
-fun ListItem(item: WeatherModel) {
+fun MainList(list: List<WeatherModel>, currentDay: MutableState<WeatherModel>) {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        itemsIndexed(
+            list
+        ) {
+                _, item -> ListItem(item, currentDay)
+        }
+    }
+}
+@Composable
+fun ListItem(item: WeatherModel, currentDay: MutableState<WeatherModel>) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 5.dp),
+            .padding(top = 5.dp).clickable {
+               if (item.hours.isEmpty()) return@clickable
+               currentDay.value = item
+            },
         backgroundColor = BlueLight,
         elevation = 0.dp,
         shape = RoundedCornerShape(5.dp)
